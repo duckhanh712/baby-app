e0e2aeb56a14e1782f146da7d035bcf288a67dc8
Khuat Duc Khanh <duckhanh97pt@gmail.com>
2024-03-06 22:06:38 +0700

    :hammer: Refactor

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,55 +21,41 @@ const topData = {
 </script>
 
 <template>
-  <div class="">
-    <div class="banner-top bg-rose-100">
-      <ul class="content flex gap-x-[28px]">
-        <li class="item cursor-pointer">Mua hàng và CSKH: 
-          <span class="text hotline"> {{ topData.hotline }} </span>
-        </li>
-        <li class="item cursor-pointer">Tìm Siêu Thị<span class="text store"> ({{ topData.totalStore }}) </span></li>
-        <li class="item cursor-pointer"><span class="mdi mdi-map-marker-outline"></span>Nhập địa chỉ để mua hàng giao <span class="text ship"> Siêu Tốc 1h </span> </li>
-        <li class="item cursor-pointer"><span class="mdi mdi-account-supervisor"></span> Đăng nhập</li>
-      </ul>
+  <div class="banner-top bg-rose-100">
+    <ul class="content flex gap-x-[28px]">
+      <li class="item cursor-pointer">Mua hàng và CSKH: 
+        <span class="text hotline"> {{ topData.hotline }} </span>
+      </li>
+      <li class="item cursor-pointer">Tìm Siêu Thị<span class="text store"> ({{ topData.totalStore }}) </span></li>
+      <li class="item cursor-pointer"><span class="mdi mdi-map-marker-outline"></span>Nhập địa chỉ để mua hàng giao <span class="text ship"> Siêu Tốc 1h </span> </li>
+      <li class="item cursor-pointer"><span class="mdi mdi-account-supervisor"></span> Đăng nhập</li>
+    </ul>
+  </div>
+  <div class="search-component flex items-center m-auto py-5">
+    <div class="w-[20%] flex justify-end	">
+      <img class=" w-[134px]" src="https://cdn1.concung.com/themes/desktop4.1/image/logo-concung.png" alt="">
     </div>
-    <div class="search-component h-16 flex items-center">
-      <div class="w-[24%] flex justify-end	">
-        <img class=" w-[134px]" src="https://cdn1.concung.com/themes/desktop4.1/image/logo-concung.png" alt="">
-      </div>
-      <form class="search-form width-conten-agoda flex m-auto justify-between">
-        <input class="input focus:outline-none" type="text">
-        <button class="search-button" color="primary">
-          <span class="mdi mdi-magnify"></span>
-        </button>
-      </form>
-      <div class="cart-area w-[20%] flex font-bold gap-x-14">
-        <div class="cart"><div class="image m-auto"></div><span class="mt-auto">Giỏ hàng</span></div>
-        <div class="noti"><div class="image m-auto"></div><span class="mt-auto">Ưu đãi</span></div>
-      </div>
-  </div>
-  </div>
+    <div>
 
+    </div>
+    <div class="search-component relative w-[52%] m-auto">
+    <form class="search-form">
+      <input class="input focus:outline-none" type="text">
+      <button class="search-button" color="primary"></button>
+    </form>
+    </div>
+    <div class="cart-area w-[20%] flex font-bold gap-x-14">
+      <div class="cart"><div class="image m-auto"></div><span class="mt-auto">Giỏ hàng</span></div>
+      <div class="noti"><div class="image m-auto"></div><span class="mt-auto">Ưu đãi</span></div>
+    </div>
+  </div>
   <NuxtPage />
 </template>
 
 <NuxtPage />
 <style lang="scss" scoped>
-.border-agoda-pink {
-  border: 1px solid rgb(var(--v-theme-agoda-pink));
-  border-radius: 5px;
-  color: rgb(var(--v-theme-agoda-pink));
-}
-
-.border-agoda-pink:hover {
-  background-color: rgb(var(--v-theme-agoda-pink));
-  color: #fff;
-}
-.nav-menu {
-  font-size: 14px;
-}
-
 .banner-top {
-  padding: 5px 10%;
+  padding: 10px 10%;
   > .content {
     gap: 10%;
     font-size: 14px;
@@ -77,19 +63,32 @@ const topData = {
   }
 }
 
+.search-component {
+  max-width: 1600px;
+}
+
 .search-form {
   .input {
-    width: 650px;
+    width: 92%;
     height: 48px;
-    border: 2px solid #ff469e;
+    border: 3px solid #ff469e;
     border-radius: 12px 0 0 12px;
-    padding: 10px;
+    padding: 10px 20px 10px 30px;
 
   }
   .search-button {
-    width: 60px;
-    background-color: #ff469e;
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 72px;
+    height: 100%;
+    background: #ff469e url(https://cdn1.concung.com/themes/desktop4.1/image/v40/icon/search.svg) no-repeat center center;
+    background-size: 28px 28px;
     border-radius: 0 12px 12px 0;
+    // width: 60px;
+    // background-color: #ff469e;
+    // border-radius: 0 12px 12px 0;
   }
 }
 
@@ -114,10 +113,6 @@ const topData = {
 }
 
 .banner-top > .content > .item {
-  .text {
-    // font-family: HelveticaNeue-Bold;
-   
-  }
   .hotline, .store {
     color: #ff469e ;
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,39 @@
 <script lang="ts" setup></script>
 
 <template>
-<div class="w-full max-w-xs">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+  <div class="container m-auto">
+    <div class="relative">
+      <ul class="category font-medium  pl-0 ">
+        <li class="flex items-center justify-between"><a href="#">Sữa bột cao cấp</a></li>
+        <li class="flex items-center justify-between"><a href="#">Bỉm Tã khuyến mãi</a></li>
+        <li class="flex items-center justify-between"><a href="#">Sữa tươi các loại</a></li>
+        <li class="flex items-center justify-between"><a href="#">Ăn dặm, dinh dưỡng</a></li>
+        <li class="flex items-center justify-between"><a href="#">Vitamin & sức khỏe</a></li>
+        <li class="flex items-center justify-between"><a href="#">Chăm sóc gia đình</a></li>
+      </ul>
     </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-      <p class="text-red-500 text-xs italic">Please choose a password.</p>
-    </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Sign In
-      </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
-    </div>
-  </form>
-  <p class="text-center text-gray-500 text-xs">
-    &copy;2020 Acme Corp. All rights reserved.
-  </p>
-</div>
+  </div>
+
 </template>
 
 <style scoped lang="scss">
+.container {
+  max-width: 1280px;
+  
+}
+.category {
+  color: #67686c;
+  font-size: 18px;
+  font-family: "HelveticaNeue-Bold" !important;
+  width: 225px;
+  background-color: rgb(255 228 230);
+  > li {
+    height: 36px;
+    width: 100%;
+    line-height: 36px;
+    display: inline-block;
+    padding-left: 20px;
+  }
+}
 
 </style>
